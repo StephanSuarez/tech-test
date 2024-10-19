e38d14bfff094e1850991c7582140b4276c02923
ste_phan <stephan102002@gmail.com>
2024-10-19 14:29:53 -0500

    working on seach products

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
 
+  constructor( private router: Router ){}
+
+  item: string = '';
+
+  search() {
+    this.router.navigate(['products'], { queryParams: { keyword: this.item } })
+  }
 }
